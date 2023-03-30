c21ba2f34ce019e35762f49bd1d45a19855fbd00
Samuel Lucić <samuelsamilucic@gmail.com>
2023-03-30 03:08:44 +0200

    homework-2

--- app/src/main/java/endava/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/endava/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
--- app/src/main/java/endava/codebase/android/movieapp/ui/component/MovieCard.kt
+++ app/src/main/java/endava/codebase/android/movieapp/ui/component/MovieCard.kt
@@ -1,5 +1,6 @@
 package endava.codebase.android.movieapp.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -14,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
