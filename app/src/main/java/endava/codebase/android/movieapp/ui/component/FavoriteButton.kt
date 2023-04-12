eda30a830c4b2c7b57e56923c9f8a368d4bb8879
Samuel Lucić <samuelsamilucic@gmail.com>
2023-04-12 09:19:14 +0200

    Rebase fix

--- app/src/main/java/endava/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/endava/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -9,8 +9,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
